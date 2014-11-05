985adbb66d79f223934161b02e78a111f97e68b0
Phillip Cave <phillip.cave@jacksonriver.com>
2014-11-05 12:17:11 -0500

    Adding inital php api client for advocacy services.

---
+++ advocacy/SpringboardAdvocacyAPIClient.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Springboard Advocacy Services API : Simple PHP wrapper for v1 of Springboard Advocacy API
+ *
+ *
+ * @category Advocacy
+ * @package  Springboard-Advocacy-API-PHP
+ * @author   Phillip Cave <phillip.cave@jacksonriver.com>
+ * @license  MIT License
+ * @link     https://github.com/JacksonRiver/springboard-api-clients/php/services/advocacy
+ */
+class SpringboardAdvocacyAPIClient
+{
+  /**
+   * The api key granted to the client application. This key
+   * will be used in all service method calls.
+   *
+   * @var string
+   */
+  private $api_key;
+
+}
